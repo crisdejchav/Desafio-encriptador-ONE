54c31fa7e4a71c0d9e4369e53cc6a7a0a70d4a54
Cristian Chavez <crisdejchav@gmail.com>
2024-08-16 23:09:55 -0500

    Concluido la mayor parte del proyecto, queda pendiente la edicion del readme

--- scripts/encriptador.js
+++ scripts/encriptador.js
@@ -1,3 +1,5 @@
+let finalMessage = "";
+
 // Objeto de claves para encriptación
 const cryptKeys = {
     "e": "enter",
@@ -11,14 +13,34 @@ const cryptKeys = {
 const inverseCryptKeys = (value) => 
     Object.keys(cryptKeys).find(key => cryptKeys[key] === value);
 
+// Función para verificar si el texto es válido
+const validateText = (text) => /^[a-z\s]+$/.test(text);
+
+/* Funcion que se encarga de procesar el mensaje para verificar que cumpla con las condiciones */
+const process = (action) => {
+    let message = document.getElementById("message").value.trim();
+    if (message == ""){
+        alertElement("info", "red", "El mensaje no puede estár vacio.");
+        return;
+    }else{
+        alertElement("info", "#495057", "Solo letras minúsculas y sin acentos.");
+    }
+    if(!validateText(message)){
+        alertElement("info", "red", "Solo letras minúsculas y sin acentos.");
+        return;
+    }
+    action == "crypt" ? crypt(message) : decrypt(message)
+
+}
 /* Funcion que encripta los mensajes */
-const crypt = () => {
-    let message = document.getElementById("message").value;
+const crypt = (message) => {
+
     let cryptedMessage = message.replace(/[eiaou]/g, match => cryptKeys[match]);
     let results = document.getElementById("results");
 
     /* Modifica y muestra los resultados */
-    results.querySelector("h2").innerText = cryptedMessage;
+    document.getElementById("results-message").innerText = cryptedMessage;
+    finalMessage = cryptedMessage;
     results.style.display = "flex";
 
     /* Oculta el mensaje de ningun mensaje fue encontrado */
@@ -26,19 +48,35 @@ const crypt = () => {
 
 }
 
-const decrypt = () => {
-    let message = document.getElementById("message").value;
+/* Funcion que se encarga de desencriptar el mensaje */
+const decrypt = (message) => {
+    
     let decryptedMessage = message.replace(/enter|imes|ai|ober|ufat/g, match => inverseCryptKeys(match));
     let results = document.getElementById("results");
 
     /* Modifica y muestra los resultados */
-    results.querySelector("h2").innerText = decryptedMessage;
+    document.getElementById("results-message").innerText = decryptedMessage;
+    finalMessage = decryptedMessage;
     results.style.display = "flex";
 
     /* Oculta el mensaje de ningun mensaje fue encontrado */
     hideNoResults();
 }
 
+const copyMessage = () =>{
+    navigator.clipboard.writeText(finalMessage);
+    alert("Mensaje copiado con exito!");
+}
+
+/* Esta funcion modifica elementos cambiando su color y su mensaje dependiendo de como sea requerido */
+const alertElement = (element, color, alertMsg) => {
+    let elem = document.getElementById(element);
+    elem.innerHTML = alertMsg;
+    elem.style.color = color;
+    return;
+}
+
+/* Esta funcion oculta el mensaje por defecto del encriptador */
 const hideNoResults = () => {
     let results = document.getElementById("no-results");
     results.style.display = "none";
